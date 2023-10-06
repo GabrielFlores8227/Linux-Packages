1969e2810184a5bea8a385fe39b1c76d1cc8cc98
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-10-05 22:58:40 -0300

    Update latest-neovim.sh

--- latest-neovim/latest-neovim.sh
+++ latest-neovim/latest-neovim.sh
@@ -47,12 +47,9 @@ function version() {
 }
 
 function installer() {
-		sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage \
-    && chmod u+x nvim.appimage \
-    && ./nvim.appimage --appimage-extract \
-    && ./squashfs-root/AppRun --version \
-    && sudo mv squashfs-root / \
-    && sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage \
+	&& chmod u+x nvim.appimage \
+	&& ./nvim.appimage
 }
 
 function executeInstaller() {
